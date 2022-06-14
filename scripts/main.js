8f93cd7f3f59b530170284497138736de18f4fc3
SlimNorris <islasmachinbrandon@gmail.com>
2022-06-14 17:01:42 -0300

    carousel functionality added

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,27 @@
-const checkbox1 = document.getElementById('checkbox1');
-checkbox1.addEventListener('change', () =>{
-        //change the theme of the website
-        document.body.classList.toggle('light')
-    }); 
+let slideIndex = 1;
+showSlides(slideIndex)
 
-const buttons = document.querySelectorAll("[data-carousel-button]")
-
-buttons.forEach(button =>{
-    button.addEventListener("click", () => {
-        const offset = button.dataset.carouselButton === "next" ? 1 : -1
-        const slides = button.closest("[data-carousel]").querySelector('[data-slides]')
-
-        const activeSlide = slides.querySelector("[data-active]")
-        let newIndex = [...slides.children].indexOf(activeSlide) + offset
-        if (newIndex < 0) newIndex = slides.children.length - 1
-        if (newIndex >= slides.children.length) newIndex = 0
-
-        slides.children[newIndex].dataset.active = true
-        delete activeSlide.dataset.active
-    })
-})
+function plusSlides(n){
+    showSlides(slideIndex += n)
+}
+function currentSlide(n){
+    showSlides(slideIndex = n)
+}
+function showSlides(n){
+    let i;
+    let slides = document.querySelectorAll(".mySlides");
+    let quadrates = document.querySelectorAll(".quadrate"); 
+    if(n > slides.length) slideIndex = 1
+    if(n < 1) slideIndex = slides.length
+    for(i = 0; i < slides.length; i++){
+        slides[i].style.opacity = "0"
+    }
+    for(i = 0; i < quadrates.length;i++){
+        quadrates[i].className = quadrates[i].className.replace(" active","")
+    }
+    slides[slideIndex-1].style.opacity = "1";
+    quadrates[slideIndex-1].className += " active";
+}
 
 var state=false;
 function toggle(){
@@ -38,6 +40,11 @@ function toggle(){
     }
 }
 
+// const checkbox1 = document.getElementById('checkbox1');
+// checkbox1.addEventListener('change', () =>{
+//         //change the theme of the website
+//         document.body.classList.toggle('light')
+//     }); 
 
 
 
